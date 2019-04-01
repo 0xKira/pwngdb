ee7acdbc903ba2e885d92b42a28ac8d96bc00b56
Kira <kira_cxy@foxmail.com>
2019-04-01 19:42:25 +0800

    from angelboy: Merge pull request #31 from bruce30262/dev Make procmap work in angelheap while using mozilla rr

--- AAApwn/angelheap.py
+++ AAApwn/angelheap.py
@@ -5,6 +5,7 @@ import gdb
 import re
 import copy
 import struct
+import os
 
 # main_arena
 main_arena = 0
@@ -350,6 +351,7 @@ def get_arch():
             word = "wx "
             arch = "arm"
             return "arm"
+
         else:
             word = "wx "
             capsize = 4
@@ -359,16 +361,41 @@ def get_arch():
         return "error"
 
 
+def infoprocmap():
+    """ Use gdb command 'info proc map' to get the memory mapping """
+    """ Notice: No permission info """
+    resp = gdb.execute("info proc map", to_string=True).split("\n")
+    resp = '\n'.join(resp[i] for i in range(4, len(resp))).strip().split("\n")
+    infomap = ""
+    for l in resp:
+        line = ""
+        first = True
+        for sep in l.split(" "):
+            if len(sep) != 0:
+                if first:  # start address
+                    line += sep + "-"
+                    first = False
+                else:
+                    line += sep + " "
+        line = line.strip() + "\n"
+        infomap += line
+    return infomap
+
+
 def procmap():
     data = gdb.execute('info proc exe', to_string=True)
     pid = re.search('process.*', data)
     if pid:
         pid = pid.group()
         pid = pid.split()[1]
-        maps = open("/proc/" + pid + "/maps", "r")
-        info_map = maps.read()
-        maps.close()
-        return info_map
+        fpath = "/proc/" + pid + "/maps"
+        if os.path.isfile(fpath):  # if file exist, read memory mapping directly from file
+            maps = open(fpath)
+            infomap = maps.read()
+            maps.close()
+            return infomap
+        else:  # if file doesn't exist, use 'info proc map' to get the memory mapping
+            return infoprocmap()
     else:
         return "error"
 
